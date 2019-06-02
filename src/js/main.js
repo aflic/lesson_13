2cf78e3c7f73b7ca55118f97de9cb6751108952a
aflic <kurapatka100@mail.ru>
2019-06-02 21:20:50 +0300

    last

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,25 @@ var button = document.querySelector("#button");
 var modal = document.querySelector("#modal");
 var close = document.querySelector("#close");
 
-button.addEventListener("click",function() {
+function closeModal(){
+  modal.classList.remove("modal_active");
+};
+
+function openModal(){
   modal.classList.add('modal_active');
+  setTimeout(closeModal, 5000);
+};
+
+button.addEventListener("click", function() {
+  openModal();
 });
 
-close.addEventListener("click",function(){
-  modal.classList.remove("modal_active")
+close.addEventListener("click", function(){
+  closeModal();
 });
+
+
+
+// setTimeout( function(){
+//   modal.classList.remove('modal_active');
+// }5000);
